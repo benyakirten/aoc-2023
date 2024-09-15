5b12ba6dd3ce741f5da43190a5867b14a425c0ab
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-09-15 00:59:04 -0600

    Complete island tests

--- day-3/island.zig
+++ day-3/island.zig
@@ -23,21 +23,21 @@ pub const Island = struct {
     pub fn fromValue(start_x: u16, value: u16, y: u16) Island {
         var num_digits: u8 = 0;
         var val = value;
-        while (val >= 0) {
+        while (val >= 10) {
             num_digits += 1;
-            val %= 10;
+            val /= 10;
         }
 
         return Island{
             .min_x = start_x,
-            .max_x = start_x + num_digits - 1,
+            .max_x = start_x + num_digits,
             .value = value,
             .y = y,
         };
     }
 };
 
-test "isAdjacentTo adjacent with the same y value" {
+test "Island.isAdjacentTo adjacent with the same y value" {
     const island = Island{ .max_x = 3, .value = 100, .min_x = 1, .y = 1 };
     const coords = [2]Coord{ Coord{ .x = 0, .y = 1 }, Coord{ .x = 4, .y = 1 } };
 
@@ -47,7 +47,7 @@ test "isAdjacentTo adjacent with the same y value" {
     }
 }
 
-test "isAdjacentTo not adjacent with the same y value" {
+test "Island.isAdjacentTo not adjacent with the same y value" {
     const island = Island{ .min_x = 5, .max_x = 7, .value = 100, .y = 1 };
     const coords = [2]Coord{ Coord{ .x = 3, .y = 1 }, Coord{ .x = 9, .y = 1 } };
 
@@ -57,7 +57,7 @@ test "isAdjacentTo not adjacent with the same y value" {
     }
 }
 
-test "isAdjacentTo diagonal positions" {
+test "Island.isAdjacentTo diagonal positions" {
     const island = Island{ .min_x = 5, .max_x = 7, .value = 100, .y = 1 };
     const coords = [4]Coord{
         Coord{ .x = 4, .y = 0 },
@@ -72,7 +72,7 @@ test "isAdjacentTo diagonal positions" {
     }
 }
 
-test "isAdjacentTo diagonal positions offset too far" {
+test "Island.isAdjacentTo diagonal positions offset too far" {
     const island = Island{ .min_x = 5, .max_x = 7, .value = 100, .y = 2 };
     const coords = [4]Coord{
         Coord{ .x = 3, .y = 1 },
@@ -85,4 +85,37 @@ test "isAdjacentTo diagonal positions offset too far" {
         const isAdjacent = island.isAdjacentTo(coord);
         try std.testing.expect(!isAdjacent);
     }
+}
+
+test "Island.isAdjacentTo within X range but Y offset" {
+    const island = Island{ .min_x = 5, .max_x = 7, .value = 100, .y = 1 };
+    const coords = [4]Coord{
+        Coord{ .x = 5, .y = 0 },
+        Coord{ .x = 6, .y = 0 },
+        Coord{ .x = 7, .y = 2 },
+        Coord{ .x = 5, .y = 2 },
+    };
+
+    for (coords) |coord| {
+        const isAdjacent = island.isAdjacentTo(coord);
+        try std.testing.expect(isAdjacent);
+    }
+}
+
+test "Island.fromValue value has 3 digits" {
+    const got = Island.fromValue(4, 100, 2);
+    const want = Island{ .min_x = 4, .max_x = 6, .value = 100, .y = 2 };
+    try std.testing.expectEqual(want, got);
+}
+
+test "Island.fromValue value has 2 digits" {
+    const got = Island.fromValue(4, 27, 2);
+    const want = Island{ .min_x = 4, .max_x = 5, .value = 27, .y = 2 };
+    try std.testing.expectEqual(want, got);
+}
+
+test "Island.fromValue value has 1 digits" {
+    const got = Island.fromValue(4, 8, 2);
+    const want = Island{ .min_x = 4, .max_x = 4, .value = 8, .y = 2 };
+    try std.testing.expectEqual(want, got);
 }
