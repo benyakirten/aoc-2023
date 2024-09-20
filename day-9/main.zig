0cfe0a1eb45b77cd729c219a5d4b8163f5945b3a
Ben Horowitz <benyakir.horowitz@gmail.com>
2024-09-19 21:52:48 -0600

    Day 9 (#8)

---
+++ day-9/main.zig
@@ -0,0 +1,108 @@
+const std = @import("std");
+
+const Sequence = @import("sequence.zig").Sequence;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    const inputs = try std.fs.cwd().openFile("puzzle_input.txt", .{ .mode = .read_only });
+    defer inputs.close();
+
+    const sequences = try readSequences(inputs, allocator);
+    defer allocator.free(sequences);
+
+    var next_total: isize = 0;
+    var previous_total: isize = 0;
+
+    for (sequences) |sequence| {
+        next_total += try sequence.getNext();
+        previous_total += try sequence.getPrevious();
+        sequence.deinit();
+    }
+
+    std.debug.print("Total for the next item: {}\n", .{next_total});
+    std.debug.print("Total for the previous item: {}\n", .{previous_total});
+}
+
+fn readSequences(file: std.fs.File, allocator: std.mem.Allocator) ![]Sequence {
+    var sequence_list = std.ArrayList(Sequence).init(allocator);
+    defer sequence_list.deinit();
+
+    var raw_sequence = std.ArrayList(u8).init(allocator);
+    defer raw_sequence.deinit();
+
+    const buf = try allocator.alloc(u8, 1);
+    defer allocator.free(buf);
+
+    while (true) {
+        const letters_read = try file.read(buf);
+
+        if (letters_read == 0 or buf[0] == '\n') {
+            const data = try raw_sequence.toOwnedSlice();
+            raw_sequence.clearAndFree();
+
+            const seq = try processSequence(data, allocator);
+            allocator.free(data);
+
+            const sequence = Sequence{ .allocator = allocator, .data = seq };
+            try sequence_list.append(sequence);
+
+            if (letters_read == 0) {
+                break;
+            }
+        } else {
+            try raw_sequence.append(buf[0]);
+        }
+    }
+
+    return try sequence_list.toOwnedSlice();
+}
+
+fn processSequence(data: []u8, allocator: std.mem.Allocator) ![]isize {
+    var items = std.ArrayList(isize).init(allocator);
+    defer items.deinit();
+
+    var item: isize = 0;
+    var is_negative: bool = false;
+    for (data, 0..) |datum, i| {
+        if (datum == ' ' or i == data.len - 1) {
+            if (i == data.len - 1) {
+                item = item * 10 + datum - '0';
+            }
+
+            if (is_negative) {
+                item *= -1;
+            }
+
+            try items.append(item);
+            item = 0;
+            is_negative = false;
+        } else if (datum == '-') {
+            is_negative = true;
+        } else {
+            item = item * 10 + datum - '0';
+        }
+    }
+
+    return try items.toOwnedSlice();
+}
+
+test "main functionality doesn't leak memory" {
+    const inputs = try std.fs.cwd().openFile("test_input.txt", .{ .mode = .read_only });
+    defer inputs.close();
+
+    const sequences = try readSequences(inputs, std.testing.allocator);
+    defer std.testing.allocator.free(sequences);
+
+    var next_total: isize = 0;
+    var previous_total: isize = 0;
+
+    for (sequences) |sequence| {
+        next_total += try sequence.getNext();
+        previous_total += try sequence.getPrevious();
+        sequence.deinit();
+    }
+}
---
+++ day-9/sequence.zig
@@ -0,0 +1,56 @@
+const std = @import("std");
+
+pub const SequenceError = error{ AllocationError, DerivationError };
+
+pub const Sequence = struct {
+    data: []isize,
+    allocator: std.mem.Allocator,
+
+    pub fn deinit(self: Sequence) void {
+        self.allocator.free(self.data);
+    }
+
+    pub fn getNext(self: Sequence) SequenceError!isize {
+        const last_item = self.data[self.data.len - 1];
+        return last_item + try deriveNextItem(self.data, self.allocator);
+    }
+
+    fn deriveNextItem(sequence: []isize, allocator: std.mem.Allocator) SequenceError!isize {
+        if (sequence.len == 0) {
+            return SequenceError.DerivationError;
+        }
+
+        var derived_sequence = allocator.alloc(isize, sequence.len - 1) catch {
+            return SequenceError.AllocationError;
+        };
+        defer allocator.free(derived_sequence);
+
+        var derived_to_nothing = true;
+        for (0..sequence.len - 1) |i| {
+            const delta: isize = sequence[i + 1] - sequence[i];
+            derived_sequence[i] = delta;
+            derived_to_nothing = derived_to_nothing and delta == 0;
+        }
+
+        if (derived_to_nothing) {
+            return 0;
+        }
+
+        return derived_sequence[derived_sequence.len - 1] + try deriveNextItem(derived_sequence, allocator);
+    }
+
+    pub fn getPrevious(self: Sequence) SequenceError!isize {
+        // Am I being too clever? It works so whatev.
+        const reversed_arr = self.allocator.alloc(isize, self.data.len) catch {
+            return SequenceError.AllocationError;
+        };
+        defer self.allocator.free(reversed_arr);
+
+        for (0..self.data.len) |i| {
+            reversed_arr[i] = self.data[self.data.len - 1 - i];
+        }
+
+        const last_item = reversed_arr[reversed_arr.len - 1];
+        return last_item + try deriveNextItem(reversed_arr, self.allocator);
+    }
+};
