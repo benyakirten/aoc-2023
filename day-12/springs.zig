f129b3f890968839bcc5dcc223b9c7ab88fdf661
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-09-22 19:30:48 -0600

    Modity method to test if a solution is possibly viable, add tests

--- day-12/springs.zig
+++ day-12/springs.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
 pub const RecordError = error{UnrecognizedLandType};
-pub const Record = enum {
-    Operational,
-    Damaged,
-    Unknown,
+pub const Record = enum(u8) {
+    Operational = '.',
+    Damaged = '#',
+    Unknown = '?',
 
     fn fromChar(char: u8) !Record {
         switch (char) {
@@ -16,26 +16,26 @@ pub const Record = enum {
     }
 };
 
-pub const KnownRecord = enum {
-    Operational,
-    Damaged,
+pub const KnownRecord = enum(u8) {
+    Operational = '.',
+    Damaged = '#',
 };
 
-pub const HotSpring = struct {
+pub const HotSprings = struct {
     records: []Record,
     positions: []u8,
     allocator: std.mem.Allocator,
 
-    pub fn deinit(self: HotSpring) void {
+    pub fn deinit(self: HotSprings) void {
         self.allocator.free(self.records);
         self.allocator.free(self.positions);
     }
 
-    pub fn parse(raw_data: []u8, allocator: std.mem.Allocator) ![]HotSpring {
+    pub fn parse(raw_data: []u8, allocator: std.mem.Allocator) ![]HotSprings {
         var hot_spring_list = std.ArrayList(u8).init(allocator);
         defer hot_spring_list.deinit();
 
-        var hot_springs_list = std.ArrayList(HotSpring).init(allocator);
+        var hot_springs_list = std.ArrayList(HotSprings).init(allocator);
         defer hot_springs_list.deinit();
 
         for (raw_data, 0..) |datum, i| {
@@ -47,7 +47,7 @@ pub const HotSpring = struct {
                 const raw_hot_spring = try hot_spring_list.toOwnedSlice();
                 hot_spring_list.clearAndFree();
 
-                const hot_spring = try HotSpring.new(raw_hot_spring, allocator);
+                const hot_spring = try HotSprings.new(raw_hot_spring, allocator);
                 try hot_springs_list.append(hot_spring);
             } else {
                 try hot_spring_list.append(datum);
@@ -57,7 +57,7 @@ pub const HotSpring = struct {
         return try hot_springs_list.toOwnedSlice();
     }
 
-    pub fn new(data: []u8, allocator: std.mem.Allocator) !HotSpring {
+    pub fn new(data: []u8, allocator: std.mem.Allocator) !HotSprings {
         var records_list = try std.ArrayList(Record).initCapacity(allocator, data.len);
         defer records_list.deinit();
 
@@ -82,17 +82,26 @@ pub const HotSpring = struct {
         const records = try records_list.toOwnedSlice();
         const positions = try positions_list.toOwnedSlice();
 
-        return HotSpring{ .records = records, .positions = positions, .allocator = allocator };
+        return HotSprings{ .records = records, .positions = positions, .allocator = allocator };
     }
 
-    // pub fn permute(self: HotSpring) void {
+    // pub fn permute(self: HotSprings) void {
     //     var index: u8 = 0;
     //     for (self.positions) |position| {}
     // }
 
-    fn isValidSolution(self: HotSpring, proposed: []KnownRecord) bool {
+    fn isPossibleSolution(self: HotSprings, proposed: []KnownRecord) bool {
         if (self.records.len != proposed.len) {
             return false;
+        }
+
+        for (0..self.records.len) |i| {
+            const record = self.records[i];
+            const proposed_item = proposed[i];
+
+            if (record != .Unknown and @intFromEnum(record) != @intFromEnum(proposed_item)) {
+                return false;
+            }
         }
 
         const groups = Groups.identify(proposed, self.allocator) catch {
@@ -104,7 +113,104 @@ pub const HotSpring = struct {
     }
 };
 
-const GroupError = error{NeedsOperationalBetweenGroup};
+test "HotSprings.isPossibleSolution will identify a correctly solution" {
+    var records = [7]Record{
+        .Unknown,
+        .Unknown,
+        .Unknown,
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Damaged,
+    };
+
+    var positions = [3]u8{ 1, 1, 3 };
+    const hot_springs = HotSprings{
+        .allocator = std.testing.allocator,
+        .positions = positions[0..],
+        .records = records[0..],
+    };
+
+    var proposed = [7]KnownRecord{ .Damaged, .Operational, .Damaged, .Operational, .Damaged, .Damaged, .Damaged };
+
+    const is_valid_solution = hot_springs.isPossibleSolution(proposed[0..]);
+    try std.testing.expect(is_valid_solution);
+}
+
+test "HotSprings.isPossibleSolution will identify a solution that does not have the correct groupings" {
+    var records = [7]Record{
+        .Unknown,
+        .Unknown,
+        .Unknown,
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Damaged,
+    };
+
+    var positions = [3]u8{ 1, 1, 3 };
+    const hot_springs = HotSprings{
+        .allocator = std.testing.allocator,
+        .positions = positions[0..],
+        .records = records[0..],
+    };
+
+    var proposed = [7]KnownRecord{ .Operational, .Damaged, .Damaged, .Operational, .Damaged, .Damaged, .Damaged };
+
+    const is_valid_solution = hot_springs.isPossibleSolution(proposed[0..]);
+    try std.testing.expect(!is_valid_solution);
+}
+
+test "HotSprings.isPossibleSolution will identify a solution that has the incorrect known records" {
+    var records = [8]Record{
+        .Unknown,
+        .Unknown,
+        .Unknown,
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Damaged,
+        .Operational,
+    };
+
+    var positions = [3]u8{ 1, 1, 3 };
+    const hot_springs = HotSprings{
+        .allocator = std.testing.allocator,
+        .positions = positions[0..],
+        .records = records[0..],
+    };
+
+    var proposed = [8]KnownRecord{ .Damaged, .Operational, .Damaged, .Operational, .Operational, .Damaged, .Damaged, .Damaged };
+
+    const is_valid_solution = hot_springs.isPossibleSolution(proposed[0..]);
+    try std.testing.expect(!is_valid_solution);
+}
+
+test "HotSprings.isPossibleSolution will identify a solution that has the incorrect amount of records" {
+    var records = [8]Record{
+        .Unknown,
+        .Unknown,
+        .Unknown,
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Damaged,
+        .Operational,
+    };
+
+    var positions = [3]u8{ 1, 1, 3 };
+    const hot_springs = HotSprings{
+        .allocator = std.testing.allocator,
+        .positions = positions[0..],
+        .records = records[0..],
+    };
+
+    var proposed = [7]KnownRecord{ .Damaged, .Operational, .Damaged, .Operational, .Damaged, .Damaged, .Damaged };
+
+    const is_valid_solution = hot_springs.isPossibleSolution(proposed[0..]);
+    try std.testing.expect(!is_valid_solution);
+}
+
 pub const Groups = struct {
     groups: []Group,
     positions: []u8,
@@ -121,24 +227,27 @@ pub const Groups = struct {
 
         var position_list = try std.ArrayList(u8).initCapacity(allocator, proposed.len / 2);
 
-        var idx = 0;
+        var idx: u8 = 0;
         while (true) {
             const group = Group.identify(proposed, idx);
+
+            // Ends with undamaged hot springs
             if (group.len == 0) {
                 break;
             }
 
-            const next_index = group.start + group.len + 1;
+            try group_list.append(group);
+            try position_list.append(group.len);
+
+            const next_index = group.start + group.len;
+            // We've gotten to the end - it ends with a damaged hot springs
             if (next_index >= proposed.len) {
                 break;
             }
 
             if (proposed[next_index] != .Operational) {
-                return GroupError.NeedsOperationalBetweenGroup;
+                unreachable;
             }
-
-            try group_list.append(group);
-            try position_list.append(group.len);
 
             idx = next_index + 1;
         }
@@ -149,6 +258,137 @@ pub const Groups = struct {
         return Groups{ .groups = groups, .positions = positions, .allocator = allocator };
     }
 };
+
+test "Groups.identify identify one group with trailing operational hot springs" {
+    var known_records = [4]KnownRecord{
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Operational,
+    };
+
+    var want_groups = [1]Group{
+        Group{
+            .start = 1,
+            .len = 2,
+        },
+    };
+
+    var want_positions = [1]u8{2};
+    const want = Groups{
+        .allocator = std.testing.allocator,
+        .groups = want_groups[0..],
+        .positions = want_positions[0..],
+    };
+
+    const got = try Groups.identify(known_records[0..], std.testing.allocator);
+    defer got.deinit();
+
+    try std.testing.expectEqualDeep(want.positions, got.positions);
+    try std.testing.expectEqualDeep(want.groups, got.groups);
+}
+
+test "Groups.identify identify one group with ends with damaged hot springs" {
+    var known_records = [3]KnownRecord{
+        .Operational,
+        .Damaged,
+        .Damaged,
+    };
+
+    var want_groups = [1]Group{
+        Group{
+            .start = 1,
+            .len = 2,
+        },
+    };
+
+    var want_positions = [1]u8{2};
+    const want = Groups{
+        .allocator = std.testing.allocator,
+        .groups = want_groups[0..],
+        .positions = want_positions[0..],
+    };
+
+    const got = try Groups.identify(known_records[0..], std.testing.allocator);
+    defer got.deinit();
+
+    try std.testing.expectEqualDeep(want.positions, got.positions);
+    try std.testing.expectEqualDeep(want.groups, got.groups);
+}
+
+test "Groups.identify identify 2 groups starting with damaged hot springs" {
+    var known_records = [4]KnownRecord{
+        .Damaged,
+        .Damaged,
+        .Operational,
+        .Damaged,
+    };
+
+    var want_groups = [2]Group{
+        Group{
+            .start = 0,
+            .len = 2,
+        },
+        Group{
+            .start = 3,
+            .len = 1,
+        },
+    };
+
+    var want_positions = [2]u8{ 2, 1 };
+    const want = Groups{
+        .allocator = std.testing.allocator,
+        .groups = want_groups[0..],
+        .positions = want_positions[0..],
+    };
+
+    const got = try Groups.identify(known_records[0..], std.testing.allocator);
+    defer got.deinit();
+
+    try std.testing.expectEqualDeep(want.positions, got.positions);
+    try std.testing.expectEqualDeep(want.groups, got.groups);
+}
+
+test "Groups.identify identify multiple groups" {
+    var known_records = [8]KnownRecord{
+        .Operational,
+        .Damaged,
+        .Operational,
+        .Damaged,
+        .Damaged,
+        .Operational,
+        .Damaged,
+        .Operational,
+    };
+
+    var want_groups = [3]Group{
+        Group{
+            .start = 1,
+            .len = 1,
+        },
+        Group{
+            .start = 3,
+            .len = 2,
+        },
+        Group{
+            .start = 6,
+            .len = 1,
+        },
+    };
+
+    var want_positions = [3]u8{ 1, 2, 1 };
+    const want = Groups{
+        .allocator = std.testing.allocator,
+        .groups = want_groups[0..],
+        .positions = want_positions[0..],
+    };
+
+    const got = try Groups.identify(known_records[0..], std.testing.allocator);
+    defer got.deinit();
+
+    try std.testing.expectEqualDeep(want.positions, got.positions);
+    try std.testing.expectEqualDeep(want.groups, got.groups);
+}
 
 pub const Group = struct {
     start: u8,
